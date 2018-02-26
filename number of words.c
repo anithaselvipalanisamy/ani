a65c02f243bba81e2d0757ca093c05302a2036cf
anithaselvipalanisamy <31499326+anithaselvipalanisamy@users.noreply.github.com>
2018-02-26 16:24:52 +0530

    Create number of words.c

---
+++ number of words.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+int i,c,l;
+char s[100];
+gets(s);
+l=strlen(s);
+for(i=0;i<l;i++)
+{
+if(s[i]==' ')
+{
+c++;
+}
+}
+printf("%d",c);
+return 0;
+}

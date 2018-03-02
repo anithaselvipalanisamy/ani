acf3c2b8df2aea624c152aeb708024f6d6cd6b87
anithaselvipalanisamy <31499326+anithaselvipalanisamy@users.noreply.github.com>
2018-03-02 16:59:47 +0530

    Create hel*o.c

---
+++ hel*o.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+char a[100];
+scanf("%s",&a);
+l=strlen(a);
+b=l/2;
+if(l%2==0)
+{
+while(a[i]==b+1)
+{
+a[i]='8'
+}
+i++;
+}
+else
+{
+while(a[i]==b)
+{
+a[i]='*'

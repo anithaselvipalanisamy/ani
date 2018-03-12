e2739a448e5214144eb43b0af76584e515bed383
anithaselvipalanisamy <31499326+anithaselvipalanisamy@users.noreply.github.com>
2018-03-12 16:48:37 +0530

    Create 13.c

---
+++ 13.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+int a;
+scanf("%d",&a);
+
+    if(a%13==0)
+    {
+        printf("yes %d",a);
+    }
+    else
+    {
+        printf("no");
+    }
+
+return 0;
+}

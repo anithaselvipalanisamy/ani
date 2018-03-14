fd399b3985b0c4dfe895f4a417682ff5b923d710
anithaselvipalanisamy <31499326+anithaselvipalanisamy@users.noreply.github.com>
2018-03-14 16:36:37 +0530

    Create ninbjans.c

---
+++ ninbjans.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c=0;
+      scanf("%d%d",&a,&b);
+        if(a>b)
+        {
+            c=a-b;
+        }
+        else
+        {
+            c=b-a;
+        }
+printf("%d",c);
+        
+   return 0;
+}
